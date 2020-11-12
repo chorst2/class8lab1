58c552d61a9198084f82f2a4a55fdd219ce63a09
Casidy Horst <CHorst2@my.wctc.edu>
2020-11-12 13:06:20 -0600

    lab

--- validation.js
+++ validation.js
@@ -1,23 +1,52 @@
 $(document).ready(function () {
     // Object containing the validation rules
     var myRules =
-        {};
+        {
+            firstName:
+                {
+                    required:true
+                },
+            grade:
+                {
+                    min:5,
+                    max:8,
+                    digits:true
+
+                }
+        };
 
     // Object containing the error messages
     var myMessages =
-        {};
+        {
+            firstName:
+                {
+                    required: "Fill this in!"
+                },
+            grade:
+                {
+                    min:"Too young",
+                    max:"Too old",
+                    digits:"Please enter a valid grade (whole number)"
+                }
+        };
 
     // Pass the configuration to the form's validate() method
     // Needs submitHandler, rules, and messages properties
     $("form").validate(
-        {}
+        {
+            submitHandler:runMyProgram,
+            rules: myRules,
+            messages: myMessages
+        }
     );
 
     function runMyProgram() {
         // Change the text of the <p> with ID of "message" to
         // state, for example, "You have registered Jane for grade 6 camp!"
         // Use the name and grade the user provided in the form.
-
+        var firstName = $("#firstName").val();
+        var grade = $("#grade").val();
+        $("#message").text(`You have registered ${firstName} for grade ${grade} camp!`);
 
     }
 
